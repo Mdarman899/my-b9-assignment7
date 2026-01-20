b46537c7f088286b14108b7e2fb0ec3e0996e199
Mdarman899 <mdaminurislam517821@gmail.com>
2026-01-20 20:43:48 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+        <h1 className='text-center text-6xl uppercase text-red-500 '>arman</h1>
+        
+    </>
+  )
+}
+
+export default App
